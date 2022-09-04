84a5f82fbd5c7bbc2339fd727db0a9d72491567e
Roman Surinov <romasurinov13@mail.ru>
2022-09-04 23:52:11 +0300

    add week filler

--- src/components/Week.jsx
+++ src/components/Week.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ArrowButton from './ArrowButton';
 import Day from './Day';
+import { fillWeek, getDayByDate } from './supportingFunctions';
 
-function Week(props) {
+function Week() {
+
+    const days = ['пн','вт','ср','чт','пт','сб','вс']
+
+    const [today,setToday] = useState(new Date())
+
+    let week = fillWeek(today) 
+    useEffect(()=>{
+        week = fillWeek(today) 
+        console.log(week);
+    },[today])
+
     return (
         <div className='week-container'>
             <ArrowButton
                 type={'prev'}
-                onClick={()=>alert('prev')}
+                onClick={()=> setToday(today.addDays(-7))}
             />
-            <Day day={'пн'} dayNumber={1}/>
+
+            {
+                week.map(
+                    (day,index)=>{
+                        return (<Day
+                            date={day}
+                            key={index}
+                        />)
+                    }
+                )
+            }
+            {/* <Day day={'пн'} dayNumber={1}/>
             <Day day={'вт'} dayNumber={2}/>
             <Day day={'ср'} dayNumber={14}/>
             <Day day={'чт'} dayNumber={15}/>
             <Day day={'пт'} dayNumber={16}/>
-            <Day day={'сб'} dayNumber={17}/>
+            <Day day={'сб'} dayNumber={17}/> */}
             <ArrowButton
                 type={'next'}
-                onClick={()=>alert('next')}
+                onClick={()=> setToday(today.addDays(7))}
             />
         </div>
     );
