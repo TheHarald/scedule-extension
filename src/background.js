6bfd46891f6e3e057f4df4275e70946fb1a353b7
Roman Surinov <romasurinov13@mail.ru>
2022-09-10 13:16:50 +0300

    add demo gif

---
+++ src/background.js
@@ -0,0 +1,7 @@
+// chrome.storage.local.clear(function() {
+//     var error = chrome.runtime.lastError;
+//     if (error) {
+//         console.error(error);
+//     }
+//     // do something more
+// });
